fef03b18f9e8960c8df9d4069b3a7347f4dc26cb
Mauro Perez <mmauroperez@gmail.com>
2019-10-31 17:04:12 -0300

    Detalles

--- src/App.vue
+++ src/App.vue
@@ -56,15 +56,10 @@
               <v-icon name="minimize-2"/>
             </div>
             <div class="tituloSubContenedor" > TRADDING</div>
-            <!-- <span >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span> -->
             <Transaciones/>
           </div>
         </div>
       </div>
-      <span style="font-size: 15px">{{ divs }}</span>
-      <br>
-      <span style="font-size: 15px">{{ width }}</span>
-
     </b-row>
   </transition>
   </div>
