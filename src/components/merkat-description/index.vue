078fa2385009da32067e08e603a44653d4c48757
Mauro Perez <mmauroperez@gmail.com>
2019-11-20 13:18:16 -0300

    funcionando

---
+++ src/components/merkat-description/index.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <span>
+    <span class="textSuperior ">ETC</span>
+    <span class="textSuperior ">BTC 0.000023</span>
+    <span class="textSuperior ">ETH 0.023434</span>
+    <span class="textSuperior ">USD 4.423</span>
+    <span class="textSuperior ">EUR 4.0657</span>
+    <span class="textSuperior ">ETC</span>
+    <span class="textSuperior ">EUR 4.0657</span>
+  </span>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+
+.textSuperior {
+  font-size: 12px;
+  padding-right: 10px;
+  border-right: 1px solid #bdb8b8;
+}
+</style>
