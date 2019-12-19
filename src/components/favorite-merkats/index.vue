84b8b27db58fc5258c606a1e30fcbcbd47f1ff96
Tomas01 <tomasvalencia510@hotmail.com>
2019-12-19 09:11:45 -0800

    Table obs2

--- src/components/favorite-merkats/index.vue
+++ src/components/favorite-merkats/index.vue
@@ -2,22 +2,21 @@
   <b-col md="20">
     <div class="tituloSubContenedor TittleStyle">Listado en observación</div>
 <table>
-  <thead>
+
+  <tbody class="borderBottom">
     <tr>
     <td> UBR/CLP</td>
     </tr>
-  </thead>
-  <tbody class="BodyStyle">
-    <tr>
+    <tr class="borderBottom">
       <td  >7487.10 USD</td>
       <td class="color-verde ">+0.64%</td>
       <td>2,017k</td>
     </tr>
 
-    <tr class="border_bottom">
+    <tr >
      <td> UBR/USD </td>
    </tr>
-   <tr>
+   <tr class="borderBottom">
      <td  >7487.10 USD</td>
      <td class="color-rojo ">+0.64%</td>
      <td>2,017k</td>
@@ -25,7 +24,7 @@
    <tr>
     <td> RPP/USD </td>
   </tr>
-  <tr>
+  <tr class="borderBottom">
     <td >7487.10 USD</td>
     <td class="color-rojo ">+0.64%</td>
     <td>2,017k</td>
@@ -33,7 +32,7 @@
   <tr>
    <td> RPI/CLP </td>
   </tr>
-  <tr>
+  <tr class="borderBottom">
    <td  >7487.10 USD</td>
    <td class="color-verde ">+0.64%</td>
    <td>2,017k</td>
@@ -60,7 +59,7 @@ td {
   border-bottom: 1px solid #1e2b375c;
 }
 
-.BodyStyle{
+.borderBottom{
   border-bottom: 1px solid #1e2b375c;
 
 }
