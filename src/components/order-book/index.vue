541994c2e466ccae83e4d76de1949d9587f4a17a
Tomaas <tomasvalencia510@hotmail.com>
2020-01-17 20:08:47 -0300

    orderBookListo

--- src/components/order-book/index.vue
+++ src/components/order-book/index.vue
@@ -1,24 +1,19 @@
 <template lang="html">
   <div>
     <div class="tableOrder noScrollY mt-2">
-
       <table class="table  table-hover  mb-0">
-
         <thead>
           <tr>
             <th>Precio</th>
             <th>Volumen</th>
             <th>Valor Total</th>
-
           </tr>
         </thead>
         <tbody >
-
           <tr v-for="(o, index) in orderPriceSell" :key='index' >
             <td class="greenColor">{{o.price}}</td>
             <td> {{o.number}} </td>
             <td> {{o.price * o.number }} </td>
-
           </tr>
         </tbody>
       </table>
@@ -27,12 +22,10 @@
     <div class="tableOrder noScrollY mt-0" >
       <table class="table  table-hover">
         <tbody >
-
           <tr v-for="(o, index) in orderPriceBuy" :key ='index' >
             <td class="redColor">{{o.price}}</td>
             <td> {{o.number}} </td>
             <td> {{o.price * o.number}} </td>
-
           </tr>
         </tbody>
       </table>
@@ -46,11 +39,10 @@
           </tr>
         </thead>
         <tbody >
-          <tr  v-for="(o, index) in orderDateCreate" >
-
-            <td>{{o.price }}</td>
-            <td>{{o.number }}</td>
-            <td>{{o.dateCreate }}</td>
+          <tr v-for="(o, index) in orderDateCreate">
+            <td>{{o.price}}</td>
+            <td>{{o.number}}</td>
+            <td>{{o.dateCreate}}</td>
           </tr>
         </tbody>
       </table>
@@ -60,39 +52,30 @@
 
 <script>
 import { mapGetters } from 'vuex';
-
 import Axios from 'axios'
 export default {
   data() {
     return {
       orders: null
     }
-
-
-
-
   },
   computed: {
     ...mapGetters({
       ordersX: 'dom/orders',
 
     }),
-
   orderPriceSell() {
     var ordersSell = this.ordersX.filter((orders) => orders.type == 'VENTA')
     return ordersSell.sort((A, B) => {console.log(A); return A.price - B.price});
   },
-
   orderPriceBuy() {
     var ordersBuy = this.ordersX.filter((orders) => orders.type == 'COMPRA')
     return ordersBuy.sort((A, B) => {console.log(A); return B.price - A.price});
   },
-
   orderDateCreate() {
     var ordersDate = this.ordersX;
-    return ordersDate.sort((A, B) =>  B.dateCreate - A.dateCreate);
+    return ordersDate.sort((A, B) =>  A.dateCreate - B.dateCreate);
   }
-
   },
   created() {
     var preformat = require('./config-data.js')
@@ -105,26 +88,19 @@ export default {
   methods: {
     shuffle() {
     },
-
-
   }
 }
 </script>
-
 <style lang="scss" scoped>
-
 .noScrollY {
   overflow-y: hidden !important;
 }
-
 .flip-list-move {
   transition: transform 1s;
 }
-
 td {
   padding: 0px !important;
 }
-
 .tableOrder {
   & > table > tbody > tr, & > table > thead > tr {
     border-bottom-color:  transparent !important;
@@ -135,15 +111,12 @@ td {
   max-height: calc(33vh - 30px) !important;
   overflow-y:auto;
 }
-
 ::-webkit-scrollbar-track {
   background: transparent !important;
 }
-
 ::-webkit-scrollbar-thumb {
   background: transparent !important;
 }
-
 .tableHistoryPrice {
   & > tbody > tr, & > thead > tr {
     border-bottom-color:  transparent !important;
@@ -154,7 +127,6 @@ td {
   overflow: auto !important;
   max-height: 200px !important;
 }
-
 .lastPrice {
   text-align:center;
   vertical-align:middle;
